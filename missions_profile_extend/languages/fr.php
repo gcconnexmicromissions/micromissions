e24dcad40c378668febf1a29a5c5e5605dd2e508
gcconnexmicromissions <david.carter@cnrc-nrc.gc.ca>
2015-11-06 14:27:50 -0500

    Language Fix
    Fixes to language files.

--- missions_profile_extend/languages/fr.php
+++ missions_profile_extend/languages/fr.php
@@ -1,17 +1,17 @@
 <?php
 $french = array(
 	// labels
-		'gcconnex_profile:opt:opt-in' => "S'Engager",
-		'gcconnex_profile:opt:set_empty' => "Aucune option d'engagement ont été sélectionnés.",
-		'gcconnex_profile:opt:micro_mission' => 'Micro Mission',
-		'gcconnex_profile:opt:job_swap' => "Échange d'Emploi",
-		'gcconnex_profile:opt:mentored' => 'Encadrés',
-		'gcconnex_profile:opt:mentoring' => 'Mentorat',
-		'gcconnex_profile:opt:shadowed' => 'Observé',
+		'gcconnex_profile:opt:opt-in' => "Options d'adhésion",
+		'gcconnex_profile:opt:set_empty' => "Aucune option d'adhésion a été sélectionnée.",
+		'gcconnex_profile:opt:micro_mission' => 'Micro missions',
+		'gcconnex_profile:opt:job_swap' => "Échange d'emploi",
+		'gcconnex_profile:opt:mentored' => 'Conseillé(e)',
+		'gcconnex_profile:opt:mentoring' => 'Conseiller/conseillère',
+		'gcconnex_profile:opt:shadowed' => 'Observé(e)',
 		'gcconnex_profile:opt:shadowing' => 'Observateur',
-		'gcconnex_profile:opt:peer_coached' => 'Pair Entraîné',
-		'gcconnex_profile:opt:peer_coaching' => 'Pair Entraineur',
-		'gcconnex_profile:opt:opt_in_access' => "Qui peut voir mes choix d'engagement :",
+		'gcconnex_profile:opt:peer_coached' => 'Pair entraîné(e)',
+		'gcconnex_profile:opt:peer_coaching' => 'Pair entraineur',
+		'gcconnex_profile:opt:opt_in_access' => "Qui peut voir mes options d'adhésion :",
 		'gcconnex_profile:opt:yes' => 'Oui',
 		'gcconnex_profile:opt:no' => 'Non'
 );
